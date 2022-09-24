e4482154f1bacdb62d0ca10a41b98e2abd75b803
janabhumi <dmitriy@ideascup.me>
2022-09-24 16:09:35 +0300

    Add overlays to flake
    Closes #17

--- flake.nix
+++ flake.nix
@@ -10,87 +10,103 @@
   };
 
   outputs = { self, flake-utils, nixpkgs, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        inherit (builtins) substring;
-        inherit (nixpkgs) lib;
+    let
+      mkNil = { rustPlatform, nix, ... }:
+        let
+          inherit (builtins) substring;
+          mtime = self.lastModifiedDate;
+          date = "${substring 0 4 mtime}-${substring 4 2 mtime}-${substring 6 2 mtime}";
+          rev = self.shortRev or (throw "Git changes are not committed");
+        in
+        rustPlatform.buildRustPackage {
+          pname = "nil";
+          version = "unstable-${date}";
+          src = self;
+          cargoLock.lockFile = self + "/Cargo.lock";
 
-        pkgs = nixpkgs.legacyPackages.${system};
-        rustPkgs = rust-overlay.packages.${system};
+          nativeBuildInputs = [ nix.out ];
 
-        pre-commit = pkgs.writeShellScriptBin "pre-commit" ''
-          set -e
-          die() { echo "$*" >&2; exit 1; }
+          CFG_DATE = date;
+          CFG_REV = rev;
+        };
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          inherit (nixpkgs) lib;
 
-          cd "$(git rev-parse --show-toplevel)"
-          rg --fixed-strings 'dbg!' --glob '*.rs' \
-            && die 'Found dbg!()'
-          cargo fmt --quiet --check >/dev/null \
-            || die 'Format failed'
-          cargo clippy --quiet --all-targets --all-features -- --deny warnings \
-            || die 'Clippy failed'
-          cargo test --quiet \
-            || die 'Test failed'
-        '';
+          pkgs = nixpkgs.legacyPackages.${system};
+          rustPkgs = rust-overlay.packages.${system};
 
-      in {
-        packages = rec {
-          default = nil;
-          nil = let
-            mtime = self.lastModifiedDate;
-            date = "${substring 0 4 mtime}-${substring 4 2 mtime}-${substring 6 2 mtime}";
-            rev = self.shortRev or (throw "Git changes are not committed");
-          in pkgs.rustPlatform.buildRustPackage {
-            pname = "nil";
-            version = "unstable-${date}";
-            src = self;
-            cargoLock.lockFile = self + "/Cargo.lock";
+          pre-commit = pkgs.writeShellScriptBin "pre-commit" ''
+            set -e
+            die() { echo "$*" >&2; exit 1; }
 
-            nativeBuildInputs = [ pkgs.nix.out ];
+            cd "$(git rev-parse --show-toplevel)"
+            rg --fixed-strings 'dbg!' --glob '*.rs' \
+              && die 'Found dbg!()'
+            cargo fmt --quiet --check >/dev/null \
+              || die 'Format failed'
+            cargo clippy --quiet --all-targets --all-features -- --deny warnings \
+              || die 'Clippy failed'
+            cargo test --quiet \
+              || die 'Test failed'
+          '';
 
-            CFG_DATE = date;
-            CFG_REV = rev;
+          nil = pkgs.callPackage mkNil { };
+        in
+        {
+          packages = {
+            inherit nil;
+            default = nil;
           };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; with rustPkgs; [
+              # Override the stable rustfmt.
+              rust-nightly_2022-08-01.availableComponents.rustfmt
+              rust
+              nix.out # For generation of builtins.
+              gdb
+              jq
+              pre-commit
+              (import ./dev/neovim-lsp.nix { inherit pkgs; })
+              (import ./dev/vim-coc.nix { inherit pkgs; })
+            ] ++ lib.optionals (lib.meta.availableOn stdenv.hostPlatform vscodium) [
+              (import ./dev/vscodium.nix { inherit pkgs; })
+            ];
+
+            RUST_BACKTRACE = "short";
+            NIXPKGS = nixpkgs;
+
+            # bash
+            shellHook = ''
+              export NIL_PATH="$(cargo metadata --format-version=1 | jq -r .target_directory)/debug/nil"
+            '';
+          };
+
+          devShells.fuzz = pkgs.mkShell {
+            packages = with pkgs; with rustPkgs; [
+              rust-nightly_2022-08-01
+              cargo-fuzz
+              llvmPackages_14.llvm
+              jq
+              gnugrep
+            ];
+            RUST_BACKTRACE = "short";
+
+            # bash
+            shellHook = ''
+              export CARGO_TARGET_DIR=~/.cache/targets-syntax
+            '';
+          };
+        })
+    // rec {
+      overlays = {
+        nil = final: prev: {
+          nil = final.callPackage mkNil { };
         };
-
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; with rustPkgs; [
-            # Override the stable rustfmt.
-            rust-nightly_2022-08-01.availableComponents.rustfmt
-            rust
-            nix.out # For generation of builtins.
-            gdb
-            jq
-            pre-commit
-            (import ./dev/neovim-lsp.nix { inherit pkgs; })
-            (import ./dev/vim-coc.nix { inherit pkgs; })
-          ] ++ lib.optionals (lib.meta.availableOn stdenv.hostPlatform vscodium) [
-            (import ./dev/vscodium.nix { inherit pkgs; })
-          ];
-
-          RUST_BACKTRACE = "short";
-          NIXPKGS = nixpkgs;
-
-          # bash
-          shellHook = ''
-            export NIL_PATH="$(cargo metadata --format-version=1 | jq -r .target_directory)/debug/nil"
-          '';
-        };
-
-        devShells.fuzz = pkgs.mkShell {
-          packages = with pkgs; with rustPkgs; [
-            rust-nightly_2022-08-01
-            cargo-fuzz
-            llvmPackages_14.llvm
-            jq
-            gnugrep
-          ];
-          RUST_BACKTRACE = "short";
-
-          # bash
-          shellHook = ''
-            export CARGO_TARGET_DIR=~/.cache/targets-syntax
-          '';
-        };
-      });
+        default = overlays.nil;
+      };
+    };
 }
