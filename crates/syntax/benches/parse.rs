e4f5e55eb984e0bf285fd1c2a58c1c6a015c49ea
oxalica <oxalicc@pm.me>
2023-07-09 02:33:48 +0800

    Add a benchmark for parsing the whole nixpkgs

--- crates/syntax/benches/parse.rs
+++ crates/syntax/benches/parse.rs
@@ -1,17 +1,22 @@
 use std::path::PathBuf;
 
-use criterion::Criterion;
+use criterion::{BatchSize, Criterion};
+use once_cell::sync::Lazy;
 
 const TEST_FILES: &[(&str, &str)] = &[
     ("gcc-13", "pkgs/development/compilers/gcc/13/default.nix"),
     ("all-packages", "pkgs/top-level/all-packages.nix"),
 ];
 
-fn bench_parser(c: &mut Criterion) {
-    let nixpkgs_path = PathBuf::from(std::env::var("NIXPKGS").expect("missing env var NIXPKGS"));
+static NIXPKGS: Lazy<PathBuf> = Lazy::new(|| {
+    std::env::var("NIXPKGS")
+        .expect("missing env var NIXPKGS")
+        .into()
+});
 
+fn bench_parser(c: &mut Criterion) {
     for &(name, path) in TEST_FILES {
-        let path = nixpkgs_path.join(path);
+        let path = NIXPKGS.join(path);
         let src = std::fs::read_to_string(path).expect("failed to read test file");
 
         c.bench_function(&format!("lex_{name}"), |b| {
@@ -23,11 +28,50 @@ fn bench_parser(c: &mut Criterion) {
             b.iter_batched(
                 || toks.clone(),
                 |toks| syntax::parser::parse_file_tokens(&src, toks),
-                criterion::BatchSize::SmallInput,
+                BatchSize::SmallInput,
             );
         });
     }
 }
 
+#[cfg(not(debug_assertions))]
+fn bench_nixpkgs(c: &mut Criterion) {
+    c.bench_function("whole_nixpkgs", |b| {
+        let srcs = walkdir::WalkDir::new(&**NIXPKGS)
+            .follow_links(false)
+            .sort_by_file_name()
+            .same_file_system(true)
+            .into_iter()
+            .filter_map(|ent| {
+                let ent = ent.expect("failed to traverse NIXPKGS");
+                if !ent.file_type().is_file()
+                    || ent.path().extension().map_or(true, |ext| ext != "nix")
+                {
+                    return None;
+                }
+                let src = std::fs::read_to_string(ent.path()).expect("failed to read file");
+                Some(src)
+            })
+            .collect::<Vec<_>>();
+        // Sanity check.
+        assert!(srcs.len() > 1000);
+
+        b.iter_batched(
+            || {},
+            |()| {
+                srcs.iter()
+                    .map(|src| syntax::parse_file(src))
+                    .collect::<Vec<_>>()
+            },
+            BatchSize::PerIteration,
+        );
+    });
+}
+
+#[cfg(debug_assertions)]
 criterion::criterion_group!(benches, bench_parser);
+
+#[cfg(not(debug_assertions))]
+criterion::criterion_group!(benches, bench_parser, bench_nixpkgs);
+
 criterion::criterion_main!(benches);
