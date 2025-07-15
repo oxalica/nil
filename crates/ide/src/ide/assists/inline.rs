9ab61c51f467e6406a1b2fabc5a629239f312a7d
Léana 江 <leana.jiang+git@icloud.com>
2025-07-16 01:37:32 +0200

    handle removal of let-in after rewrite

--- crates/ide/src/ide/assists/inline.rs
+++ crates/ide/src/ide/assists/inline.rs
@@ -58,11 +58,11 @@ pub(super) fn inline_from_definition(ctx: &mut AssistsCtx<'_>) -> Option<()> {
     let ptr = AstPtr::new(&covering_node.syntax());
     let name_id = source_map.name_for_node(ptr)?;
 
-    let binding_definition = covering_node
+    let binding_pathvalue = covering_node
         .syntax()
         .ancestors()
-        .find_map(ast::AttrpathValue::cast)
-        .and_then(|path_value| path_value.value())?;
+        .find_map(ast::AttrpathValue::cast)?;
+    let binding_definition = binding_pathvalue.value()?;
 
     let binding_usages = name_res
         .iter()
@@ -72,24 +72,31 @@ pub(super) fn inline_from_definition(ctx: &mut AssistsCtx<'_>) -> Option<()> {
         })
         .collect::<Vec<_>>();
 
-    let edits = binding_usages
-        .iter()
-        .map(|usage| {
-            let usage_node = usage.to_node(&ctx.ast.syntax());
-            let replacement_text = maybe_parenthesize(&binding_definition, &usage_node);
-
-            TextEdit {
-                delete: usage.text_range(),
-                insert: replacement_text,
-            }
+    let mut rewrites: Vec<TextEdit> = vec![];
+    let parent = binding_pathvalue.syntax().parent()?;
+    let is_letin = ast::LetIn::cast(parent.clone()).is_some();
+    // FIXME: this would delete into the following expression
+    if is_letin {
+        rewrites.push(TextEdit {
+            delete: parent.text_range(),
+            insert: Default::default(),
         })
-        .collect::<Vec<_>>();
+    };
+    for usage in binding_usages {
+        let usage_node = usage.to_node(&ctx.ast.syntax());
+        let replacement_text = maybe_parenthesize(&binding_definition, &usage_node);
+
+        rewrites.push(TextEdit {
+            delete: usage.text_range(),
+            insert: replacement_text,
+        })
+    }
 
     ctx.add(
         "inline_from_definition",
         format!("Inline binding `{}`", covering_node.syntax().text()),
-        AssistKind::RefactorInline,
-        edits,
+        AssistKind::RefactorRewrite,
+        rewrites,
     );
 
     Some(())
@@ -111,7 +118,7 @@ mod tests {
     use expect_test::expect;
 
     #[test]
-    fn let_in() {
+    fn let_in_ref() {
         define_check_assist!(super::inline_from_reference);
         check(
             r#"let a = "foo"; in $0a"#,
@@ -120,7 +127,7 @@ mod tests {
     }
 
     #[test]
-    fn let_in_lambda() {
+    fn let_in_lambda_ref() {
         define_check_assist!(super::inline_from_reference);
         check(
             "let a = x: x; in $0a 1",
@@ -129,7 +136,7 @@ mod tests {
     }
 
     #[test]
-    fn attr() {
+    fn attr_ref() {
         define_check_assist!(super::inline_from_reference);
         check(
             "rec { foo = 1; bar = $0foo; }",
@@ -138,7 +145,7 @@ mod tests {
     }
 
     #[test]
-    fn replace_from_definition() {
+    fn let_in_def() {
         define_check_assist!(super::inline_from_definition);
         check("let $0a = x: x; in a a", expect!["(x: x) (x: x)"]);
     }
